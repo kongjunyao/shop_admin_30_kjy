7f65f6f57fcdd10a3e186d3368a9db45e62d3e0c
kongjunyao <1021356597@qq.com>
2018-12-27 15:29:52 +0800

    home组件完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,68 +1,101 @@
 <template>
-
-<el-container class="home">
-  <el-header>
-    <div class="logo"></div>
-    <div class="title">电商后台管理系统</div>
-    <div class="logout">欢迎光临<a href="javascript:;" @click="logout">退出</a></div>
-  </el-header>
-  <el-container>
-    <el-aside width="200px">Aside</el-aside>
-    <el-main>Main</el-main>
+  <el-container class="home">
+    <el-header>
+      <div class="logo"></div>
+      <div class="title">电商后台管理系统</div>
+      <div class="logout">
+        欢迎光临
+        <a href="javascript:;" @click="logout">退出</a>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          default-active="1-1"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span slot="title">权限管理</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
   </el-container>
-</el-container>
-
 </template>
 
 <script>
 export default {
   methods: {
-    logout () {
+    logout() {
       localStorage.removeItem('token')
       this.$router.push('/login')
       this.$message.success('退出系统成功')
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
- .home{
-   height: 100%;
-   .el-header{
-      height: 60px;
-      background-color: #B3C1CD;
-      display: flex;
-      .logo{
-        width: 180px;
-        background-image: url('~@/assets/logo.png');
-        background-size: contain;
-        background-repeat: no-repeat;
-        background-position: center center;
+.home {
+  height: 100%;
+  .el-header {
+    height: 60px;
+    background-color: #b3c1cd;
+    display: flex;
+    .logo {
+      width: 180px;
+      background-image: url('~@/assets/logo.png');
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position: center center;
+    }
+    .logout {
+      width: 180px;
+      line-height: 60px;
+      font-weight: 700;
+      text-align: right;
+      a {
+        color: orange;
       }
-      .logout{
-        width: 180px;
-        line-height: 60px;
-        font-weight: 700;
-        text-align: right;
-        a{
-          color: orange;
-        }
-      }
-      .title{
-        flex: 1;
-        line-height: 60px;
-        text-align: center;
-        font-size: 30px;
-        color: #fff;
-        font-weight: 700;
-      }
-   }
-   .el-aside{
-      background-color: #545C64;
-   }
-   .el-main{
-      background-color: #D4DFE4;
-   }
- }
+    }
+    .title {
+      flex: 1;
+      line-height: 60px;
+      text-align: center;
+      font-size: 30px;
+      color: #fff;
+      font-weight: 700;
+    }
+  }
+  .el-aside {
+    background-color: #545c64;
+    .el-submenu {
+      width: 200px;
+    }
+  }
+  .el-main {
+    background-color: #d4dfe4;
+  }
+}
 </style>
